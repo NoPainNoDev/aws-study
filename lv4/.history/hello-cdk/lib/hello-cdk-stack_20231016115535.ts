5529bd8af458f5166e862ee0da6d24d996f6e0a8
lipcoder <sb921204@naver.com>
2023-10-16 13:08:26 +0900

    add cdk

---
+++ lv4/.history/hello-cdk/lib/hello-cdk-stack_20231016115535.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    // add bucket
+    new cdk.aws_s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+      bucketName: "someBucketName",
+    });
+  }
+}
